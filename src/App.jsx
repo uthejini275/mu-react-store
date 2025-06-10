9c77cfd155bad6592ddcdeafb5c3767de90ba74f
uthejini275 <uthejini275petti@gmail.com>
2025-06-10 14:27:10 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,30 @@
-import { useState } from "react";
+import { useState, createContext } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
-import Login from "./login";
-import Register from "./register";
+import Login from "./Login";
+import Register from "./Register";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Home";
+export const AppContext = createContext();
 function App() {
+  const [users,setUsers] = useState([])
   return (
     <div>
-      <BrowserRouter>
-        <Header name="mu-react-store" />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Routes>
-        <Footer />
-      </BrowserRouter>
+      <AppContext.Provider value={{users,setUsers}}>
+        <BrowserRouter>
+          <Header name="mu-react-store" />
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </AppContext.Provider>
     </div>
   );
 }
